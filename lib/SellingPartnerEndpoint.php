5438c7edcf188752cebaadc01af591f3ccfceac3
hmdev <hmdev2020@gmail.com>
2021-05-12 23:59:15 +0800

    added sandbox url

--- lib/SellingPartnerEndpoint.php
+++ lib/SellingPartnerEndpoint.php
@@ -7,4 +7,8 @@ class SellingPartnerEndpoint
     public static $EUROPE = 'https://sellingpartnerapi-eu.amazon.com';
     public static $FAR_EAST = 'https://sellingpartnerapi-fe.amazon.com';
     public static $NORTH_AMERICA = 'https://sellingpartnerapi-na.amazon.com';
+
+    const SANDBOX_NORTH_AMERICA = 'https://sandbox.sellingpartnerapi-na.amazon.com';
+    const SANDBOX_EUROPE        = 'https://sandbox.sellingpartnerapi-eu.amazon.com';
+    const SANDBOX_FAR_EAST      = 'https://sandbox.sellingpartnerapi-fe.amazon.com';
 }
